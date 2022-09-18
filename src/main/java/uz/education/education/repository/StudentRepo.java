5cf7049af08539c819a060fcfeadfcd2faac0a18
Akbar <akbar358888@gmail.com>
2022-09-18 10:50:29 +0500

    run

---
+++ src/main/java/uz/education/education/repository/StudentRepo.java
@@ -0,0 +1,15 @@
+package uz.education.education.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import uz.education.education.models.Active;
+import uz.education.education.models.user.Student;
+
+import java.util.List;
+
+public interface StudentRepo extends JpaRepository<Student, Long> {
+
+    @Query("select s from Student s where s.active =:active")
+    List<Student> findAll(@Param("active") Active active);
+}
---
+++ src/main/java/uz/education/education/service/StudentService.java
@@ -0,0 +1,39 @@
+package uz.education.education.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uz.education.education.models.Active;
+import uz.education.education.models.user.Student;
+import uz.education.education.repository.StudentRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StudentService {
+
+    final StudentRepo studentRepo;
+
+    public List<Student> getAllActive() {
+        return studentRepo.findAll(Active.ACTIVE);
+    }
+
+    public Student save(Student student) {
+        return studentRepo.save(student);
+    }
+
+    public Student remove(String id) {
+        Optional<Student> studentOptional = studentRepo.findById(Long.parseLong(id));
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            student.setActive(Active.NOACTIVE);
+            return student;
+        } else {
+            return null;
+        }
+
+    }
+}
